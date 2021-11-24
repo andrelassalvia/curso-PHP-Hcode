154ca89127efd2137632c12b61a39df6ee6ae99a
Andre Lassalvia <andrelassalvia@gmail.com>
2021-11-24 13:00:52 +0000

    Arrays Inicio

---
+++ 5-Arrays/3-Array_constante.php
@@ -0,0 +1,12 @@
+<?php
+
+// Para definir uma constante (nome, valor). Constantes usam letras MAIUSCULAS.
+define('SERVIDOR', '127.0.0.1');
+
+// array constante
+define('SERVIDOR',[
+  '127.0.0.1',
+  'usuario',
+  'root',
+  'teste'
+]);
